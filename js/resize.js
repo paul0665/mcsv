2d3dac1a303e378e6ba2d64b52952fed6e5afa1e
admin <paul002706@gmail.com>
2020-11-27 13:52:33 +0800

    resize.js

--- js/resize.js
+++ js/resize.js
@@ -2,6 +2,7 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 // 當使用者向下滑動80px後 縮小導行列
 window.onscroll = function() {scrollFunction()};
+window.onresize = function () {rwd_Phone()};
 
 function scrollFunction() {
   if(document.body.clientWidth<580){
@@ -9,13 +10,18 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "25px";
     return;
   }
-
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+function rwd_Phone(){
+  if(document.body.clientWidth<580){
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
   }
 }
 
--- js/rwd.js
+++ js/rwd.js
@@ -1,7 +1,16 @@
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("navbar").style.display = "none";
 }
 
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("navbar").style.display = "initial";
+    //document.getElementById("mySidepanel").addEventListener(ontransitionend(setSideInitial()))
+
+}
+
+function setSideInitial(){
+    if(document.getElementById("mySidepanel").is .finished)
+    document.getElementById("navbar").style.display = "initial";
 }
