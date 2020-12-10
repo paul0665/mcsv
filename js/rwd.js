0d01a12f45cec4a03569b82723018ac227c6b59f
admin <paul002706@gmail.com>
2020-12-10 14:49:38 +0800

    Dark Mode

--- js/rwd.js
+++ js/rwd.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("mySidepanel").style.width = "500px";
     document.getElementById("MainNavbar").style.display = "none";
 }
 
