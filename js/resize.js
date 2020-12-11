6f1e9dfc8c985f3db4a473fa111d5c80212852b8
admin <paul002706@gmail.com>
2020-12-11 11:19:56 +0800

    Css fix

--- js/resize.js
+++ js/resize.js
@@ -1,15 +1,15 @@
 ﻿
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-// 當使用者向下滑動80px後 縮小導行列
+
 window.onscroll = function() {scrollFunction()};
 window.onresize = function () {rwd_Phone()};
 
 function scrollFunction() {
   if(document.body.clientWidth<1500){
-    document.getElementById("MainNavbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("logo").style.fontSize = "50px";
     return;
   }
+  // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+  // 當使用者向下滑動80px後 縮小導行列
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("MainNavbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
@@ -19,9 +19,9 @@ function scrollFunction() {
   }
 }
 function rwd_Phone(){
-  if(document.body.clientWidth<580){
+  if(document.body.clientWidth<1500){
     document.getElementById("MainNavbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("logo").style.fontSize = "50px";
   }
 }
 
