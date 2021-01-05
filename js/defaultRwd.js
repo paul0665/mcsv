190f44a160f81826122289fca25ee2aae51a2872
admin <paul002706@gmail.com>
2021-01-05 14:22:56 +0800

    working on default

--- js/defaultRwd.js
+++ js/defaultRwd.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)  2020 paul0665(as know as paul)
+ * Copyright (C)  2021 paul0665(as know as paul)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -8,37 +8,8 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-* {box-sizing: border-box;}
-
-body {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-.mainsite-content1 a{
-
-}
-
-.mainsite-content1 {
-
-}
-
-.floatLeft{
-    float: left;
-    width: auto;
-}
-.floatRight{
-    float: right;
-    width: auto;
-}
-/* 小於1500px */
-@media screen and (max-width: 1500px) {
-    .floatLeft{
-        float: none;
-        width: 100%;
-    }
-    .floatRight{
-        float: none;
-        width: 100%;
-    }
+window.load +=  setTimeout(    function (){setFullWidthWhenPhone();},600);
+function setFullWidthWhenPhone(){
+    if(document.body.clientWidth<1500)
+    document.getElementById("fullWidthWhenPhone").style.width = "100%";
 }
